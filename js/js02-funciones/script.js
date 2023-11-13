5f85986cd3ee0e75b0ca2458a88ddfff535d84f3
Sergio Torres <sergio.torres@idr.edu.mx>
2023-11-13 16:54:13 -0600

    feat: Funciones expresadas y definidas

--- js/js02-funciones/script.js
+++ js/js02-funciones/script.js
@@ -1,1 +1,53 @@
 console.log("Sesion JS02 - funciones");
+
+// ---------------- Función declarada, funciones definidas-------------
+//    ( function declaration, function statement)
+/*
+  Una de las características de las funcioens declaradas
+  es que tienen hoisting (pueden ser llamadas antes de su declaració).
+*/
+
+multiplica(5, 3); // 15
+
+/**
+ * Comentario de función.
+ * Multiplica dos números e imprime en consola.
+ * @param {number} a valor de multiplicando.
+ * @param {number} b valor de multiplicador.
+ */
+function multiplica( a, b ){
+    const resultado = a * b;
+    console.log(resultado);
+}
+
+function divide( dividendo , divisor){
+    return dividendo / divisor;
+}
+
+console.log( divide(5 , 2) ); // 2.5
+console.log( divide(5 , "2") ); // 2.5
+console.log( divide("5" , "2") ); // 2.5 
+console.log( divide("5 0" , "2") ); // NaN
+
+// ---------------- Función expresadas-------------
+//    ( function expressions)
+/*
+  Funciones que son declaradas dentro de la asignación de una variable.
+  Estas funciones pueden ser anónimas ( no tiene nombre).
+
+  Las funciones expresadas no tiene hoisting, por que no se carga
+  en memoria hasta que se utilice.
+*/
+
+const sum = function sumaDeEnteros( a , b) {
+    return a + b;
+};
+
+console.log( sum( 6, 20)); // 26
+
+const potencia = function( base, exponente){
+    return base**exponente;
+};
+
+console.log( potencia(2,3)); // 8
+
--- js/js02-funciones/var-let-const.js
+++ js/js02-funciones/var-let-const.js
@@ -51,3 +51,81 @@ functionScope();
 // console.log(myVar); // myVar is not defined
 // console.log(myLet); // myLet is not defined
 // console.log(myConst); // myConst is not defined
+
+
+// ================ Block Scope ====================
+/*
+  Variables declaradas con let y const tienen un alcance de bloque. 
+  Esto significa que están limitadas al bloque en el que se declaran.
+*/
+{
+    var a = "a";
+    let b = "b";
+    const c = "c"; 
+
+    console.log(a);
+    console.log(b);
+    console.log(c);
+}
+
+console.log(a); // a
+// console.log(b); //  b is not defined
+// console.log(c); //  c is not defined
+
+var personaA = "Nathanael";
+let personaB = "Peso Pluma";
+const personaC = "Junior H";
+{
+    var personaA = "Margarita";
+    let personaB = "Ángeles Azules";
+    const personaC = "Alacranes Musicales"
+    {
+        var personaA = "Tatiana";
+        let personaB = "La Granja de Zenón";
+        const personaC = "Cri-cri";
+
+        console.log(personaA); // Tatiana 
+        console.log(personaB); // La Granja de Zenón
+        console.log(personaC); // Cri-cri
+    }
+}
+
+console.log(personaA); // Tatiana 
+console.log(personaB); // Peso Pluma 
+console.log(personaC); // Junior H
+
+
+// ================ Hoisting =======================
+/*
+  El "hoisting" en JavaScript es un comportamiento que ocurre durante 
+  la fase de compilación antes de la ejecución del código. 
+  Se refiere a la elevación (movimiento) de las declaraciones 
+  de variables y funciones al inicio de su contexto de ejecución
+
+*/
+
+
+/*
+  Las variables declaradas con var son elevadas al inicio de su contexto 
+  de ejecución (ya sea el ámbito de función o el ámbito global).
+  
+  Sin embargo, solo la declaración de la variable es elevada, 
+  no la asignación. Esto significa que la variable existe, 
+  pero su valor es undefined hasta que llega a la línea de asignación.
+*/
+console.log( phone ); // undefiend
+
+var phone;
+phone = "55-56-123-123";
+console.log( phone ); // "55-56-123-123"
+
+/*
+  Las variables declaradas con let y const también se elevan al 
+  inicio de su contexto, pero a diferencia de var, no se inicializan 
+  automáticamente con undefined. La fase de inicialización de estas 
+  variables no ocurre hasta llegar a la línea de código donde se declaran.
+
+*/
+
+// console.log( email ); // Cannot access 'email' before initialization
+let email = "tan@gamil.com"
