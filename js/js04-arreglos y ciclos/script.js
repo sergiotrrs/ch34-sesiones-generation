ffd2283a2525b02336b330c766ff76d1ffc29fe1
Sergio Torres <sergio.torres@idr.edu.mx>
2023-11-16 12:00:00 -0600

    feat: uso de break y continue

--- js/js04-arreglos y ciclos/script.js
+++ js/js04-arreglos y ciclos/script.js
@@ -57,3 +57,69 @@ const equiposIntegrador = [
     ["Gaby"  , "Charly", "Alan"   , "Sebas" , "Andrew"  , "Arlene" , "Arely"]  
 ];
 */
+for (let equipo = 0 ; equipo < equiposIntegrador.length; equipo++){
+   console.log(equiposIntegrador[equipo] );
+   for (let persona = 0; persona< equiposIntegrador[equipo].length; persona++ ){
+     console.log( equiposIntegrador[equipo][ persona ]);
+   }
+}
+
+// Usando el método forEach
+console.log("======== forEach========")
+
+function imprimirFilas( element, index, array ){
+    console.log( element );
+}
+
+// equiposIntegrador.forEach( imprimirFilas );
+// equiposIntegrador.forEach( function impEquipos(element, index, array){ console.log( element );});
+// equiposIntegrador.forEach( function (element, index, array){ console.log( element );});
+// equiposIntegrador.forEach( (element, index, array) => console.log( element ) );
+
+equiposIntegrador.forEach( element => console.log( element ) );
+
+// equiposIntegrador.forEach( fila => fila.forEach( columna => console.log(columna) ));
+equiposIntegrador.forEach( equipo => equipo.forEach( persona => console.log(persona) ));
+
+equiposIntegrador.forEach( (equipo, indiceEquipo) => {
+    equipo.forEach( persona => console.log(indiceEquipo, persona) )
+});
+
+// ======== Iterar usando For of ============
+// Se usa en elementos iterables como arreglos o strings
+console.log("====== for of ========");
+for(let fila of equiposIntegrador ){
+    for(let columna of fila){
+        console.log( columna );
+    }
+}
+
+const myName = "Alejandro";
+for (let char of myName){
+    console.log(char);
+}
+
+// ============== break y continue ==========
+/*
+  Consideraciones con el ciclo for:
+  No es necesario indicar la expresión inicial, la evaluación y la expresión final
+
+for (inicio; compración; ExpFinal){
+    sentecias;
+}
+
+for( ; ; ){
+
+}
+
+break : rompe con la iteración en curso y finaliza el ciclo for
+
+*/
+const numbers = [3,6,8,1];
+// Detener la iteración si encontramos el número 8
+for (let number of numbers ){
+    console.log( number );
+    if( number === 8 ) break;
+}
+
+const matrix = [ [2,4,6], [6,8,9], [2,6,8,3] ];
