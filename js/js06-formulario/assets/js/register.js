ce47afee59f32708176682b3a1e58d2d1b41e573
Sergio Torres <sergio.torres@idr.edu.mx>
2023-11-21 12:58:49 -0600

    ci: mensaje de error

--- js/js06-formulario/assets/js/register.js
+++ js/js06-formulario/assets/js/register.js
@@ -19,10 +19,10 @@ registerForm.addEventListener( "submit" , ( event )=>{
     console.log(event);
 
     const user = {
-        email : "sergio@gmail.com",
-        password : "123",
-        confirmPassword : "123",
-        termsandconditions : true
+        email : registerForm.elements["inputEmail4"].value ,
+        password : registerForm.elements["inputPassword4"].value ,
+        confirmPassword : registerForm.elements["inputPassword5"].value,
+        termsandconditions : registerForm.elements["gridCheck"].checked
     }
 
     if( datosVerificados( user )  ){
@@ -32,8 +32,40 @@ registerForm.addEventListener( "submit" , ( event )=>{
 } );
 
 const datosVerificados = ( user ) => {
-    // TODO verificar datos
-    return true;
+    let response = true;
+
+    if( user.email === "" ){
+        mensajeError("Falta introducir el email");
+        response = false;
+    } else if( user.password.length < 8) {
+        mensajeError("El password debe ser mayor o igual a 8 caracteres");
+        response = false;
+    } else if( user.password !== user.confirmPassword) {
+        mensajeError("La confirmación de contraseña no coincide");
+        response = false;
+    } else if ( !user.termsandconditions ) {
+        mensajeError("Debe aceptar los términos y condiciones");
+        response = false;
+    } else {
+        mensajeError("");
+    }
+
+    return response;
+}
+
+const mensajeError = ( message ) =>{
+    console.log( message);
+    const errorMessage = document.getElementById("error-message");
+    const errorMessageContainer = document.getElementById("error-message-container");
+
+    errorMessage.innerHTML = message;
+    if( message === "" ){
+        // displaty: none: quita el elemento del DOM
+        // visibility: hidden : ocultar visualmente el elemento
+        errorMessageContainer.style.display = "none";
+    } else{
+        errorMessageContainer.style.display = "block";
+    }
 }
 
 const enviarDatosAlServidor = ( user ) => {
