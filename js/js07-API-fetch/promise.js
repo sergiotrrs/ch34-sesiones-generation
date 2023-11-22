a5163026f2db32cbb116f81703b461e7c47ddc9e
Sergio Torres <sergio.torres@idr.edu.mx>
2023-11-22 17:47:32 -0600

    feat: uso de async y await

--- js/js07-API-fetch/promise.js
+++ js/js07-API-fetch/promise.js
@@ -28,13 +28,11 @@ sintaxis:
 
 // const pinkyPromise = new Promise( ()=>{} );
 // const pinkyPromise = new Promise( (fncCallBackResolve, fncCallBackReject)=>{} );
-const pinkyPromise = new Promise( (  fncCallBackResolve, fncCallBackReject )=>{
-    const isTeenager = false;
+const pinkyPromise = new Promise((fncCallBackResolve, fncCallBackReject) => {
+  const isTeenager = false;
 
-    if(  isTeenager  )
-        fncCallBackResolve( "Hay que estar siempre juntos" );
-    else
-        fncCallBackReject( { code: 404, message : "Ni te topo"} );
+  if (isTeenager) fncCallBackResolve("Hay que estar siempre juntos");
+  else fncCallBackReject({ code: 404, message: "Ni te topo" });
 });
 
 console.log("=====> Promesas <======");
@@ -52,35 +50,33 @@ console.log("Antes de consumir la promesa");
                             //después de que la promesa fue resuelta o rechazada
  */
 pinkyPromise
-    .then( response => console.log(response) )
-    .catch( error => console.log(error)  )
-    .finally( ()=> console.log("Función de Finally")  );
+  .then((response) => console.log(response))
+  .catch((error) => console.log(error))
+  .finally(() => console.log("Función de Finally"));
 
 console.log("Después de consumir la promesa");
 
-
 // =======================================
 
-const greeting = ( name ) =>{
+const greeting = (name) => {
+  // const myPromise = new Promise( ()=>{} );
+  const myPromise = new Promise((resolve, reject) => {
+    // saludar a la persona pero:
+    // si su nombre comienza con A, retardar 5 s.
+    // si comienza co G, retardar 2 s.
+    // caso contrario, rechazar la promesa
+    if (name.charAt(0) === "a" || name.charAt(0) === "A")
+      setTimeout(() => resolve(`Hola ${name}`), 5000);
+    else if (name.charAt(0).toLowerCase() === "g")
+      setTimeout(() => resolve(`Hi ${name}`), 2000);
+    else reject(`Te vi, pero te ignoré ${name} `);
+  });
 
-    // const myPromise = new Promise( ()=>{} );
-    const myPromise = new Promise( ( resolve, reject )=>{
-        // saludar a la persona pero:
-        // si su nombre comienza con A, retardar 5 s.
-        // si comienza co G, retardar 2 s.
-        // caso contrario, rechazar la promesa
-        if( name.charAt(0) === 'a' || name.charAt(0) === 'A')
-             setTimeout( ()=>resolve(`Hola ${name}`) , 5000  );
-        else if( name.charAt(0).toLowerCase() === 'g'  )
-            setTimeout( ()=>resolve(`Hi ${name}`)   , 2000 );
-        else
-            reject( `Te vi, pero te ignoré ${name} `);
-    } );
-
-    return myPromise;
-}
+  return myPromise;
+};
 
 // greeting("Antonio").then().catch().finally();
+/*
 greeting("Antonio")
  .then( response => console.log( response ))
  .catch( error => console.log(error ));
@@ -93,3 +89,63 @@ greeting("Sergio")
  .then( response => console.log( response ))
  .catch( error => console.log(error ));
  
+ */
+//---------------- consumir las promesas con async y await-------------
+/*
+ Async y Await facilita la escritura y lectura de código asíncrono.
+ Permite escribir código asíncrono de manera similar a cómo se escribiría
+ código síncrono.
+
+*/
+
+async function greetingCh34(name) {
+  console.log(">>> Inicio de saludo");
+  const result = await greeting(name);
+  console.log(result);
+  console.log("<<< Fin de saludo");
+}
+
+const greettingAll = async () => {
+  await greetingCh34("Luis");
+  await greetingCh34("Alan");
+  await greetingCh34("Geo");
+};
+
+/**
+ * Utilizar para cada función asíncrona un bloque try-catch.
+ * En caso de que la promesa sea rechazada, se ejecuta el
+ * código en el bloque catch(){} pero no detiene la ejecución
+ * de las otras funciones asíncronas.
+ */
+const greettingAllTryCatch = async () => {
+  try {
+    await greetingCh34("Luis");
+  } catch (error) {
+    console.log(error);
+  }
+  try {
+    await greetingCh34("Alan");
+  } catch (error) {
+    console.log(error);
+  }
+  try {
+    await greetingCh34("Geo");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const greettingAllUsingTryCatch = async () => {
+  try {
+    await greetingCh34("Alan");
+    await greetingCh34("Geo");
+    await greetingCh34("Luis");
+  } catch (error) {
+    console.log("La promesa fue rechazada");
+    console.warn(error);
+  }
+};
+
+// greettingAll();
+// greettingAllUsingTryCatch();
+greettingAllTryCatch();
