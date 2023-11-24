920100162e9b3888310e106f733b0352188d220d
Sergio Torres <sergio.torres@idr.edu.mx>
2023-11-24 11:33:08 -0600

    ci: generacion de clase gato

--- js/js08-OOP/cat-class.js
+++ js/js08-OOP/cat-class.js
@@ -22,17 +22,31 @@ class Felino {
    comer() {
     return `Soy ${this.nombre} y estoy comiendo`;
    }
+}
 
-
+/**
+ *  Usando la palabra reservada extends podemos heredar 
+ *  atributos y métodos de otra clase.
+ */
+class Gato extends Felino {
+    
+   constructor( nombre, size, domesticado ) {
+      super(nombre, 4, true, size);
+      this.domesticado = domesticado;
+   }
 }
 
 
 // Instanciar la clase Felino
-const chicharron = new Felino("Chicharron");
-const wero = new Felino("Wero");
-const pelusa = new Felino("Pelusa");
+const chicharron = new Felino("Chicharron", 4, true, "grande");
+const wero = new Felino("Wero", 4, true, "grande");
+const pelusa = new Felino("Pelusa", 4, true, "chica");
 
 // Uso de la función comer
 console.log(  chicharron.comer()  );
 console.log(  wero.comer()  );
 console.log(  pelusa.comer()  );
+
+// Instanciar la clase gato
+const naranjo = new Gato("Naranjo", "grande");
+
