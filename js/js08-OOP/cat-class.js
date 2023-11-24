b40f573c5e90c8425232b5410318bffc63515bc0
Sergio Torres <sergio.torres@idr.edu.mx>
2023-11-24 12:26:51 -0600

    ci: encapsular atributo createdAt

--- js/js08-OOP/cat-class.js
+++ js/js08-OOP/cat-class.js
@@ -9,6 +9,8 @@ class Felino {
    // numPatas
    // tieneGarras
    // size
+   // Atributo encapsulado
+   #createdAt;
 
    // El método constructor nos ayuda a inicializar los atributos de un objeto
    constructor ( name, numPatas, tieneGarras, size ) {
@@ -16,11 +18,29 @@ class Felino {
       this.numPatas = numPatas;
       this.tieneGarras = tieneGarras;
       this.size = size;
+      this.#createdAt = new Date().getTime(); // Obtener el valor en milisegundos
       console.log(`Soy ${this.nombre} y tengo vida, ja - ja- ja`)
    }
 
+   // Setters and Getters
+   get createdAt(){
+      return this.#createdAt;
+   }
+   /*
+   set createdAt( newDate ){
+      this.#createdAt = newDate;
+   }*/
+
    comer() {
     return `Soy ${this.nombre} y estoy comiendo`;
+   }
+   
+   /**
+    * Tiempo de vida del animal desde la fecha de creación hasta la fecha actual.
+    * @returns tiempo de vida en ms.
+    */
+   lifeSpan(){
+      return new Date().getTime() - this.#createdAt;
    }
 }
 
@@ -30,7 +50,7 @@ class Felino {
  */
 class Gato extends Felino {
     
-   constructor( nombre, size, domesticado ) {
+   constructor( nombre, size, domesticado = true ) {
       super(nombre, 4, true, size);
       this.domesticado = domesticado;
    }
@@ -49,4 +69,26 @@ console.log(  pelusa.comer()  );
 
 // Instanciar la clase gato
 const naranjo = new Gato("Naranjo", "grande");
+const kiara = new Gato("Kiara", "mediana");
+const tsuki = new Gato("Tsuki", "grande", false);
+
+console.log(  naranjo.comer() );
+console.log(  kiara.comer() );
+console.log(  tsuki.comer() );
+
+console.log( `${ naranjo.nombre} es un gato ${ naranjo.domesticado ? "domesticado": "salvaje"} `)
+console.log( `${ tsuki.nombre} es una gata ${ tsuki.domesticado ? "domesticada": "salvaje"} `)
+
+// Accediendo a un atributo encapsulado
+
+// No se puede acceder directamente sobre un atributo encapsulado
+/*
+console.log( `Tiempo de vida de ${ naranjo.nombre} : 
+               ${ naranjo.#createdAt}`);
+*/
+
+setTimeout( 
+   ()=> console.log( `Tiempo de vida de ${ naranjo.nombre} : ${ naranjo.lifeSpan()}`)  
+   ,2000 );
+
 
