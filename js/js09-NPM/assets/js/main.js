b70545813da8d0b111a07453326982ee6d26b40f
Sergio Torres <sergio.torres@idr.edu.mx>
2023-11-24 17:02:16 -0600

    ci: solicitud get con api fetch

--- js/js09-NPM/assets/js/main.js
+++ js/js09-NPM/assets/js/main.js
@@ -1,1 +1,19 @@
 console.log("JS09-NPM");
+
+const url = "https://fakestoreapi.com/users";
+
+const getUsersUsingFetch = async ( url ) => {
+  // realizar la solicitud HTTP get, usando la API FETCH
+  try {
+    const response = await fetch(url);
+    const users = await response.json();
+
+    console.log( users);
+
+  } catch (error) {
+    console.log(error);
+  }
+
+}
+
+getUsersUsingFetch( url );
