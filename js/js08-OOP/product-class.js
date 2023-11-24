a80a32f8b6bfe0034a30070998ce32757faf0429
Sergio Torres <sergio.torres@idr.edu.mx>
2023-11-24 12:44:11 -0600

    ejercicio: clase producto

---
+++ js/js08-OOP/product-class.js
@@ -0,0 +1,13 @@
+/* Crear la clase producto. Atributos enncapsulados, Crear Setters y Getters
+   * Utilizar la herencia.
+        "id": 2,
+        "title": "Mens Casual rts ",
+        "price": 22.3,
+        "description": "Slies a three-button placket.",
+        "category": "men's clothing",
+        "image": "https://fakestoreap.jpg",
+        "rating": {
+            "rate": 4.1,
+            "count": 259
+        }
+*/
