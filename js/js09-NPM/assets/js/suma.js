14c486d809d6b0e940d223e9926166487d2939f9
Sergio Torres <sergio.torres@idr.edu.mx>
2023-11-27 12:24:43 -0600

    ci: pruebas unitarias de números decimales

--- js/js09-NPM/assets/js/suma.js
+++ js/js09-NPM/assets/js/suma.js
@@ -3,7 +3,16 @@
 // Función suma que tenga de entradas dos parámetros
 // salida con la sumatoria.
 
-const suma = (a,b) => a + b ;
+
+//const suma = (a,b) => a + b ; // Errores de precisiòn con num. decimales
+const suma = (a,b) => {          // a: .1 , b: .2
+    const numAEntero = a * 1000; // .1 * 1000 = 100
+    const numBEntero = b * 1000; // .2 * 1000 = 200
+    const result = numAEntero + numBEntero; // 100 + 200 = 300
+    return result / 1000; //   300 / 1000 = .30 
+    
+    // return (a * 1000 + b * 1000) / 1000;
+} ;
 
 console.log( suma(5,3) ); // 8
 
--- js/js09-NPM/assets/js/test/suma.test.js
+++ js/js09-NPM/assets/js/test/suma.test.js
@@ -8,3 +8,21 @@ test("Prueba de números enteros" , ()=>{
     expect( suma(1_000, 2_000) ).toBe(3_000);
 });
 
+// Prueba de números negativos
+test("Prueba de números negativos" , ()=>{
+    expect( suma( -5, -3 ) ).toBe( -8 );
+    expect( suma( -10 , 3) ).toBe( -7 );
+    expect( suma( -1_000, 2_000) ).toBe( 1_000 );
+});
+
+// Prueba de números decimales
+ test("Prueba de números decimales", ()=>{
+    expect( suma( .1 , .2 )).toBe( .3 );
+    expect( suma( .1 , .3 )).toBe( .4 );
+    expect( suma( .1 , .4 )).toBe( .5 );
+    expect( suma( .1 , .5 )).toBe( .6 );
+    expect( suma( .1 , .6 )).toBe( .7 );
+    expect( suma( .1 , .7 )).toBe( .8 );
+    expect( suma( .1 , .8 )).toBe( .9 );
+    expect( suma( .1 , .9 )).toBe( 1.0 );
+ } );
