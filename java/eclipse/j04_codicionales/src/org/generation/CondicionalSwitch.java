4d13a487517434bc8b5d1962fdaca93ac3e29bc6
Sergio Torres <sergio.torres@idr.edu.mx>
2023-12-05 11:22:19 -0600

    solucio: mes de nacimiento

--- java/eclipse/j04_codicionales/src/org/generation/CondicionalSwitch.java
+++ java/eclipse/j04_codicionales/src/org/generation/CondicionalSwitch.java
@@ -1,5 +1,7 @@
 package org.generation;
 
+import java.util.Scanner;
+
 public class CondicionalSwitch {
 
 	public static void main(String[] args) {
@@ -25,6 +27,73 @@ public class CondicionalSwitch {
 		 * salida-> Naciste en diciembre.
 		 * 
 		 */
+		Scanner myScan = new Scanner(System.in);
+		// Scanner sc = new Scanner(System.in);
+
+		System.out.println("Introduce tu numero de mes: ");
+		if (myScan.hasNextInt()) { // Verifica si existe un valor numerico
+			int numeroMes = myScan.nextInt();
+			String nombreMes;
+			switch (numeroMes) {
+			case 1:
+				nombreMes = "ENERO";
+				break;
+			case 2:
+				nombreMes = "FEBRERO";
+				break;
+			case 3:
+				nombreMes = "MARZO";
+				break;
+			case 4:
+				nombreMes = "ABRIL";
+				break;
+			case 5:
+				nombreMes = "MAYO";
+				break;
+			case 6:
+				nombreMes = "JUNIO";
+				break;
+			case 7:
+				nombreMes = "JULIO";
+				break;
+			case 8:
+				nombreMes = "AGOSTO";
+				break;
+			case 9:
+				nombreMes = "SEPTIEMBRE";
+				break;
+			case 10:
+				nombreMes = "OCTUBRE";
+				break;
+			case 11:
+				nombreMes = "NOVIEMBRE";
+				break;
+			case 12:
+				nombreMes = "DICIEMBRE";
+				break;
+			default:
+				nombreMes = "NO EXISTE ESE MES";
+				break;
+			}
+			System.out.println("Tu mes de nacimiento es: " + nombreMes);
+			myScan.nextLine(); // limpiar la terminal
+			
+			// Verificar la estación del mes.
+			// ej: La estación es invierno.
+			// 12, 1, 2 : invierno
+			// 3, 4, 5 : primavera
+			// 6, 7, 8 : verano
+			// 9, 10, 11 : otoño
+			
+			
+		} else {
+			System.out.println("No introdujiste un numero entero ");
+			myScan.nextLine(); // limpiar la terminal
+		}
+		
+		
+		myScan.close();
+		
 		
 
 	}
