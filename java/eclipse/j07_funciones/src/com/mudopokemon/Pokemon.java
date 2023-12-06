9de61e972e7a8549e10c5aa6497ef19d88a96e21
Sergio Torres <sergio.torres@idr.edu.mx>
2023-12-06 12:47:54 -0600

    ci: sobrecarga de metodos

--- java/eclipse/j07_funciones/src/com/mudopokemon/Pokemon.java
+++ java/eclipse/j07_funciones/src/com/mudopokemon/Pokemon.java
@@ -14,8 +14,42 @@ public class Pokemon {
         System.out.println(nombre + " duermiendo....");
     }
     
+   
+    
+    /**
+     * Sobrecarga de Métodos (Method Overloading)
+     * 
+     * Es la capacidad de definir múltiples métodos con el
+     * mismo nombre en una clase. Esto permite usar el mismo
+     * nombre para operaciones similares pero con diferentes
+     * tipos de datos o parámetros.
+     * 
+     * Reglas:
+     *  - El nombre del método debe ser el mismo
+     *  - La lista de parámetros o número deben ser diferentes
+     *  - El retorno puede ser diferente
+     * 
+     */
     void comer() {
         System.out.println(nombre + " comiendo....");
+    }
+    
+    /**
+     * Alimento para que coma el pokemon
+     * @param alimento
+     */
+    void comer(String alimento) {
+    	System.out.println(nombre + " come " + alimento);
+    }
+    
+    /**
+     * Alimento y gramaje que comerá el pokemon
+     * @param alimento para el pokemon
+     * @param gramaje del alimento
+     * @return
+     */
+    String comer(String alimento, int gramaje) {
+    	return nombre + "comer " + gramaje +" gr. de " + alimento;
     }
     
     //metodos de clase
