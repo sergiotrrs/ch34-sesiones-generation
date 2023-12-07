24cd626e27e86a71b9f8f1023a8658ef675d4e1a
Sergio Torres <sergio.torres@idr.edu.mx>
2023-12-07 15:12:54 -0600

    ci: clase stream

--- java/eclipse/j08_colecciones/src/org/generation/HashSetConceptos.java
+++ java/eclipse/j08_colecciones/src/org/generation/HashSetConceptos.java
@@ -53,8 +53,58 @@ public class HashSetConceptos {
 		// Iteramos la colección con for each y lambda expressions
 		paises.forEach(pais -> System.out.println(pais));
 		
+		// Iterando con  Double Colon Operator (Operador de referencia)
+		/*
+		 *  El operador double colon :: u operador de referencia 
+		 *  de metodo es algo que se introdujo con java 8 para 
+		 *  hacer que el código sea mas simple y legible. 
+		 *  Lo que hace es abreviar una expresión lambda 
+		 *  para llamar a cualquier método.
+		 */
+		System.out.println("===== Double Colon Operator =====");
+		// paises.forEach(pais -> System.out.println(pais));
+		paises.forEach( System.out :: println);
 		
-
+				
+		// Usando Streams con colecciones:
+		/*
+		 *  Los streams proporcionan una forma de trabajar con 
+		 *  secuencias de elementos de datos de manera eficiente y 
+		 *  funcional. Java introdujo el concepto de streams en Java 8 
+		 *  como parte del paquete java.util.stream.
+		 *  
+		 *  Un stream en Java tiene las siguientes características clave:
+		 *  
+		 *  - Secuencialidad: Los elementos se procesan uno a la vez 
+		 *  en orden, de principio a fin.
+		 *  
+		 *  - Funcionalidad: Los streams proporcionan una interfaz funcional 
+		 *  para realizar operaciones sobre los datos, como filtrado, mapeo 
+		 *  y reducción. Esto permite un estilo de programación más funcional.
+		 *  
+		 *  - Perezoso (Lazy): Los streams son perezosos, lo que significa 
+		 *  que no realizan ninguna operación hasta que se les solicita. 
+		 *  Esto permite la optimización de recursos al evitar el 
+		 *  procesamiento innecesario.
+		 *  
+		 *  - Pipeline: Los streams admiten operaciones en cadena, 
+		 *  conocidas como pipelines, donde se pueden realizar varias 
+		 *  operaciones en una secuencia.
+		 *  
+		 *  
+		 */
+		paises
+			.stream() // Conversión a clase Stream
+			.map(String::length) // Obteniendo la longitud de cada elemento String
+			.forEach(System.out::println); // Imprimiendo en consola
+		
+		// Lo anterior usando lambda expression
+		System.out.println("---> lambda expressions <---");
+		paises
+			.stream()
+			.map( pais -> pais.length() )
+			.forEach( paisLength -> System.out.println("País : " + paisLength));
+                         // con la expresión lambda se permite personalizar la salida
 	}
 
 }
