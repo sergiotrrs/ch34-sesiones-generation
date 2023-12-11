c25ff4664754799c0c88d3ae5d00a40b78fe65b4
Sergio Torres <sergio.torres@idr.edu.mx>
2023-12-11 12:30:13 -0600

    ci: metodo generateSerialNumber

--- java/eclipse/j09-OOP/src/com/santander/Atm.java
+++ java/eclipse/j09-OOP/src/com/santander/Atm.java
@@ -8,8 +8,50 @@ public class Atm {
 	
 	// atributos de clase (static fields)
 	static String bankName = "BBVA";
+	static int counter = 0;
 	
-	// método constructor
+	/**
+	 * Método constructor
+	 * 
+	 * El método constructor se utiliza para iniciar 
+	 * objetos de una clase.
+	 * El constructor se llama automáticamente cuando 
+	 * se crea una instancia
+	 * de la clase utilizando la pabra reservada new.
+	 * Su propósito es asignar valores iniciales a 
+	 * los objetos.
+	 * 
+	 * Características:
+	 *  - Su nombre es idéntico al de la clase.
+	 *  - No tiene un tipo de retorno explícito.
+	 *  - Puede tener parámetros
+	 *  - Puede ser sobrecargado (method Overloading)
+	 *  
+	 *  Constructor por default: no tiene parámetros.
+	 *  Si no se agrega algún constructor
+	 *	java proporciona este método.
+	 */
+	 Atm(){
+		 System.out.println("Me estoy instanciando");
+		 // this.serialNumber = ++counter;
+		 generateSerialNumber();
+	 }
+	 
+	 /**
+	  * Constructor sobrecargado
+	  * Los métodos sobrecargados tienen el mismo nombre
+	  * pero diferentes parámetros.
+	  * 
+	  * @param balance cantidad disponible del cajero
+	  */
+	 Atm(double balance){
+		 this.balance = balance;
+		 generateSerialNumber();
+	 }
+	 
+	 int generateSerialNumber() {
+		 return this.serialNumber = ++counter;
+	 }
 	
 	// métodos de instancia
 	
@@ -19,15 +61,18 @@ public class Atm {
 		// Creamos el cajero01 y asignarle $1000 al balance
 		Atm cajero01 = new Atm();
 		cajero01.balance = 1000;
-		cajero01.serialNumber = 1;
+		// cajero01.serialNumber = 1;
 		System.out.println( cajero01.bankName );
 		System.out.println( Atm.bankName ); // recomendable
 		
 		// Creamos el cajero02 y asignarle $2000 al balance
 		Atm cajero02 = new Atm();    
 		cajero02.balance = 2000;
-		cajero02.serialNumber = 2;
+		// cajero02.serialNumber = 2;
 		System.out.println( cajero02.bankName );
+		
+		// Creamos el cajero 03 y asignamos $3000 al balance
+		Atm cajero03 = new Atm(3_000);
 		
 	}
 }
