71800883d28d69bacebcb7915f2bf9beb56e903b
Sergio Torres <sergio.torres@idr.edu.mx>
2023-12-12 13:04:33 -0600

    ci: metodos deposito y retiro

--- java/eclipse/j10-encapsulacion/src/com/citibanamex/Atm.java
+++ java/eclipse/j10-encapsulacion/src/com/citibanamex/Atm.java
@@ -30,6 +30,42 @@ public class Atm {
 		this.serialNumber = serialNumber;
 	}
 	
+	public double getBalance(){
+		return this.balance;
+	}
 	
+	private void setBalance(double balance) { 
+		this.balance = balance;
+	}
+	
+	public void setBalance(String balance) {
+		setBalance( Double.parseDouble(balance) );
+	}
+	
+	public void deposit(double amount ) {
+		if ( amount > 0  ) {
+			setBalance( getBalance() + amount );
+		} else {
+			System.out.println("No se permite depósitos igual o menor a cero");
+		}		
+	}
+	
+	public void deposit(String amount) {
+		deposit(Double.parseDouble(amount));
+	}
+	
+	// Verificar que el amount no sea negativo o mayor al balance
+	// enviar un mensaje en consola de acuerdo a la situación.
+	public void withdraw(double amount ) {
+		if( amount <= 0 ) {
+			System.out.println("Cantidad menor o igual a cero");
+		} else if( amount > getBalance() ) {
+			System.out.println("Fondos insuficientes");
+		} else {
+			setBalance( getBalance() - amount  );
+			// this.balance -= amount;
+		}
+		
+	}
 	
 }
--- java/eclipse/j10-encapsulacion/src/org/generation/Bank.java
+++ java/eclipse/j10-encapsulacion/src/org/generation/Bank.java
@@ -10,16 +10,16 @@ public class Bank {
 		Atm cajero02 = new Atm(2_000);
 		Atm cajero03 = new Atm("3000");
 		
-		System.out.println("Cajero01: " + cajero01.serialNumber);
-		System.out.println("Cajero02: " + cajero02.serialNumber);
-		System.out.println("Cajero03: " + cajero03.serialNumber);
+		System.out.println("Cajero01: " + cajero01.getSerialNumber() );
+		System.out.println("Cajero02: " + cajero02.getSerialNumber() );
+		System.out.println("Cajero03: " + cajero03.getSerialNumber() );
 		
-		cajero01.serialNumber = 2;
-		cajero03.serialNumber = 2;
+		cajero01.setBalance("3000");
+		cajero02.setBalance(-400);
+		cajero03.setBalance( cajero03.getBalance() - 1_000_000 );
+		
+		
 
-		System.out.println("Cajero01: " + cajero01.serialNumber);
-		System.out.println("Cajero02: " + cajero02.serialNumber);
-		System.out.println("Cajero03: " + cajero03.serialNumber);
 		
 	}
 
