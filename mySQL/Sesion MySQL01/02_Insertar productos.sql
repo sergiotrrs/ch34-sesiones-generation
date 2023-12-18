b2f5b1d6f45ab63d117f39c8dbb71df2d334b1cd
Sergio Torres <sergio.torres@idr.edu.mx>
2023-12-18 14:11:05 -0600

    ci: insertar productos

---
+++ mySQL/Sesion MySQL01/02_Insertar productos.sql
@@ -0,0 +1,16 @@
+/*
+ Sesion MySQL 01
+ Insertar productos
+*/
+
+-- Insertar registros indicando el id
+INSERT INTO productos (id, nombre, precio, categoria) VALUES (1, "Mango", 25, "Frutas");
+INSERT INTO productos (id, nombre, precio, categoria) VALUES (2, "Limón", 30, "Frutas");
+
+-- Insertar registros sin indicar el id
+INSERT INTO productos (nombre, precio, categoria) VALUES ("Bicicleta", 5000, "Deportes");
+INSERT INTO productos (nombre, precio, categoria) VALUES ("Lavadora", 7000, "Línea Blanca");
+INSERT INTO productos (nombre, precio, categoria) VALUES ("Taladro", 2500, "Herramientas");
+INSERT INTO productos (nombre, precio, categoria) VALUES ("Desarmador", 50, "Herramientas");
+
+SELECT * FROM productos;
