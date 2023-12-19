7f747925a20744c9b10f712d8e2010da8e553da8
Sergio Torres <sergio.torres@idr.edu.mx>
2023-12-19 16:21:12 -0600

    ci: inserts compras

---
+++ mySQL/Sesion MySQL02/08_Inserts compras.sql
@@ -0,0 +1,6 @@
+USE tienda;
+DESCRIBE compras;
+
+INSERT INTO compras(usuario_id, monto_total, fecha_compra) VALUES (12, 0, "2023-12-01");
+
+SELECT * FROM compras;
---
+++ mySQL/Sesion MySQL02/09_Inserts compras_has_productos.sql
@@ -0,0 +1,6 @@
+SELECT * FROM tienda.compras_has_productos;
+
+INSERT INTO compras_has_productos(compras_id, productos_id, cantidad, subtotal, descuento, total )
+   VALUES(1, 6,  4, 65, 0, 260);
+INSERT INTO compras_has_productos(compras_id, productos_id, cantidad, subtotal, descuento, total )
+   VALUES(1, 14,  4, 70, 0, 280);
