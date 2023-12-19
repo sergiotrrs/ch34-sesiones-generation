eb757492af3d4d9e522c0b10da1ddf46a874265f
Sergio Torres <sergio.torres@idr.edu.mx>
2023-12-19 16:28:11 -0600

    ci: actualizar nacimiento de Tan

--- mySQL/Sesion MySQL02/08_Inserts compras.sql
+++ mySQL/Sesion MySQL02/08_Inserts compras.sql
@@ -4,3 +4,8 @@ DESCRIBE compras;
 INSERT INTO compras(usuario_id, monto_total, fecha_compra) VALUES (12, 0, "2023-12-01");
 
 SELECT * FROM compras;
+
+-- Modificar el valor del monto total
+UPDATE compras SET monto_total = 540 WHERE id = 1;
+-- Modificar la fecha de nacimiento de Tania
+UPDATE usuarios SET fecha_nacimiento = "1997-03-09 01:05:00" WHERE id=19;
