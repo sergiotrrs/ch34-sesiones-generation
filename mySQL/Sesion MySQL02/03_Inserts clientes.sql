cdb69cfe431bd6ccfb9b2d0a3ac2b76fd54ce5c8
Sergio Torres <sergio.torres@idr.edu.mx>
2023-12-19 13:05:44 -0600

    ci: insert clientes

--- mySQL/Sesion MySQL02/03_Inserts clientes.sql
+++ mySQL/Sesion MySQL02/03_Inserts clientes.sql
@@ -2,5 +2,128 @@ DESCRIBE usuarios;
 -- Insertar nuevos usuarios
 INSERT INTO usuarios(nombre, apellido, direccion, telefono, email, fecha_nacimiento, rol_id)
    VALUES("Sergio", "Torres", "Siempre viva 100", "5555556610", "torres@gmail.com", "1983-12-08", 3);
-   
+
+-- Inserts de Valdo
+INSERT usuarios(nombre,apellido,direccion,telefono,email,fecha_nacimiento,rol_id)
+        VALUES("Oswaldo","Fonseca","Beberly Hills 90210","55123890","oswaldoFTK@gmail.com","1996-08-11",3);
+INSERT usuarios(nombre,apellido,direccion,telefono,email,fecha_nacimiento,rol_id)
+        VALUES("Nana","Fonseca","Beberly Hills 90210","55321456","nana_gorda@gmail.com","2015-08-27",1);
+INSERT usuarios(nombre,apellido,direccion,telefono,email,fecha_nacimiento,rol_id)
+        VALUES("Itzel","Fonseca","Beberly Hills 90210","55999000","itzel@gmail.com","1999-03-15",1);
+INSERT usuarios(nombre,apellido,direccion,telefono,email,fecha_nacimiento,rol_id)
+        VALUES("Aerendil","El Alto","Minas Thirit hasta arriba","55777999","AerendilElAlto@gmail.com","1985-08-11",4);
+INSERT usuarios(nombre,apellido,direccion,telefono,email,fecha_nacimiento,rol_id)
+        VALUES("Keanu","Reeves","Toronto, Ostario","55785643","Soy.JohnWick@gmail.com","1997-09-02",5);
+INSERT usuarios(nombre,apellido,direccion,telefono,email,fecha_nacimiento,rol_id)
+        VALUES("Abel","Makkonen","Scarborough toronto","55153289","TheWeekend@gmail.com","1990-02-16",1);
+INSERT usuarios(nombre,apellido,direccion,telefono,email,fecha_nacimiento,rol_id)
+        VALUES("Benjamin","Burnley","Atlantic City 1843","55090807","BreakinBenjamin@gmail.com","1978-03-10",4);
+INSERT usuarios(nombre,apellido,direccion,telefono,email,fecha_nacimiento,rol_id)
+        VALUES("Jason Joel","Desrouleaux","Miramar Florida 90210","55565452","JasonDerulo@gmail.com","1989-09-21",2);
+INSERT usuarios(nombre,apellido,direccion,telefono,email,fecha_nacimiento,rol_id)
+        VALUES("Tom","Bombadil","La comarca 2345","55302010","TomBombadil@gmail.com","1970-01-01",2);
+INSERT usuarios(nombre,apellido,direccion,telefono,email,fecha_nacimiento,rol_id)
+        VALUES("Cristiano","Ronaldo","Nassr 4352 Arabia Saudita","55454948","CR7_oficial@gmail.com","1985-02-05",1);
+
+-- Inserts de Mirta
+    
+    INSERT INTO usuarios(nombre, apellido, direccion, telefono, email, fecha_nacimiento, rol_id)
+    VALUES(
+    "Mirta",
+    "Tapia",
+    "Avellanitas 2799",
+    "1234567890",
+    "mirta_atd@hotmail.com",
+    "1994-07-31 08:35",
+    2
+    );
+    
+    INSERT INTO usuarios(nombre, apellido, direccion, telefono, email, fecha_nacimiento, rol_id)
+    VALUES(
+    "Misael",
+    "Zepeda",
+    "Amo a Tony 9000",
+    "1234567890",
+    "misael.zepedadiaz@gmail.com",
+    "1994-02-23",
+    3
+    );
+    
+    INSERT INTO usuarios(nombre, apellido, direccion, telefono, email, fecha_nacimiento, rol_id)
+    VALUES(
+    "Jorge",
+    "Ramirez",
+    "A todo gas 2001",
+    "1234567890",
+    "jorgermzc16@gmail.com",
+    "1994-10-16",
+    4
+    );
+    
+    INSERT INTO usuarios(nombre, apellido, direccion, telefono, email, fecha_nacimiento, rol_id)
+    VALUES(
+    "Yazmin (Yaz)",
+    "Mora",
+    "Holitas 213",
+    "1234567890",
+    "yazminvaleriamoralesm@gmail.com",
+    "1995-01-29",
+    5
+    );
+    
+    INSERT INTO usuarios(nombre, apellido, direccion, telefono, email, fecha_nacimiento, rol_id)
+    VALUES(
+    "Antonio (Tony)",
+    "Gonzalez",
+    "Toninator 2412",
+    "1234567890",
+    "navarrete.antonio.2301@gmail.com",
+    "1994-01-23",
+    3
+    );
+    
+    INSERT INTO usuarios(nombre, apellido, direccion, telefono, email, fecha_nacimiento, rol_id)
+    VALUES(
+    "Jocelyn",
+    "Peña",
+    "Woof Woof 3257",
+    "1234567890",
+    "jocelyn.p.reyes332b@gmail.com",
+    "1997-05-07",
+    5
+    );
+    
+    INSERT INTO usuarios(nombre, apellido, direccion, telefono, email, fecha_nacimiento, rol_id)
+    VALUES(
+    "Tania",
+    "Gayosso",
+    "Policía de Perritos 9985",
+    "1234567890",
+    "tania.gayosso@idr.edu.mx",
+    "1994-01-01",
+    1
+    );
+    
+    INSERT INTO usuarios(nombre, apellido, direccion, telefono, email, fecha_nacimiento, rol_id)
+    VALUES(
+    "Sahary",
+    "Escobedo",
+    "Boopito 56",
+    "1234567890",
+    "sahary.escobedo@generation.org",
+    "1996-01-01",
+    1
+    );
+    
+    INSERT INTO usuarios(nombre, apellido, direccion, telefono, email, fecha_nacimiento, rol_id)
+    VALUES(
+    "Nancy",
+    "Moreno",
+    "Calorcito 23",
+    "1234567890",
+    "nancy.moreno@generation.org",
+    "1996-01-01",
+    1
+    );
+
 SELECT * FROM usuarios;
