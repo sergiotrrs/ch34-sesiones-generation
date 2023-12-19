b7df4dfb61843fd3872fb74b0d2c567b970bb088
Sergio Torres <sergio.torres@idr.edu.mx>
2023-12-19 17:10:53 -0600

    backuo: datos en compras

--- mySQL/Sesion MySQL02/08_Inserts compras.sql
+++ mySQL/Sesion MySQL02/08_Inserts compras.sql
@@ -2,6 +2,16 @@ USE tienda;
 DESCRIBE compras;
 
 INSERT INTO compras(usuario_id, monto_total, fecha_compra) VALUES (12, 0, "2023-12-01");
+INSERT INTO compras(usuario_id, monto_total, fecha_compra) VALUES (2, 0, "2023-12-01");
+INSERT INTO compras(usuario_id, monto_total, fecha_compra) VALUES (2, 0, "2023-12-02");
+INSERT INTO compras(usuario_id, monto_total, fecha_compra) VALUES (3, 0, "2023-12-03");
+INSERT INTO compras(usuario_id, monto_total, fecha_compra) VALUES (4, 0, "2023-12-04");
+INSERT INTO compras(usuario_id, monto_total, fecha_compra) VALUES (5, 0, "2023-12-05");
+INSERT INTO compras(usuario_id, monto_total, fecha_compra) VALUES (6, 0, "2023-12-06");
+INSERT INTO compras(usuario_id, monto_total, fecha_compra) VALUES (7, 0, "2023-12-07");
+INSERT INTO compras(usuario_id, monto_total, fecha_compra) VALUES (8, 0, "2023-12-08");
+INSERT INTO compras(usuario_id, monto_total, fecha_compra) VALUES (9, 0, "2023-12-09");
+INSERT INTO compras(usuario_id, monto_total, fecha_compra) VALUES (10, 0, "2023-12-10");
 
 SELECT * FROM compras;
 
--- mySQL/Sesion MySQL02/09_Inserts compras_has_productos.sql
+++ mySQL/Sesion MySQL02/09_Inserts compras_has_productos.sql
@@ -1,6 +1,63 @@
 SELECT * FROM tienda.compras_has_productos;
 
-INSERT INTO compras_has_productos(compras_id, productos_id, cantidad, subtotal, descuento, total )
-   VALUES(1, 6,  4, 65, 0, 260);
-INSERT INTO compras_has_productos(compras_id, productos_id, cantidad, subtotal, descuento, total )
-   VALUES(1, 14,  4, 70, 0, 280);
+
+-- Compra 1
+INSERT INTO compras_has_productos(compras_id, productos_id, cantidad, subtotal, descuento, total)
+VALUES(1, 6, 4, 65, 0, 260);
+INSERT INTO compras_has_productos(compras_id, productos_id, cantidad, subtotal, descuento, total)
+VALUES(1, 14, 4, 70, 0, 280);
+
+-- Compra 2
+-- Compra 3
+INSERT INTO compras_has_productos(compras_id, productos_id, cantidad, subtotal, descuento, total)
+VALUES(3, 8, 3, 65, 0, 195);
+INSERT INTO compras_has_productos(compras_id, productos_id, cantidad, subtotal, descuento, total)
+VALUES(3, 12, 2, 80, 0, 160);
+
+
+INSERT INTO compras_has_productos(compras_id, productos_id, cantidad, subtotal, descuento, total)
+VALUES(3, 10, 5, 55, 0, 275);
+INSERT INTO compras_has_productos(compras_id, productos_id, cantidad, subtotal, descuento, total)
+VALUES(3, 16, 1, 70, 0, 70);
+
+-- Compra 4
+INSERT INTO compras_has_productos(compras_id, productos_id, cantidad, subtotal, descuento, total)
+VALUES(4, 18, 2, 65, 0, 130);
+INSERT INTO compras_has_productos(compras_id, productos_id, cantidad, subtotal, descuento, total)
+VALUES(4, 20, 3, 90, 0, 270);
+
+-- Compra 5
+INSERT INTO compras_has_productos(compras_id, productos_id, cantidad, subtotal, descuento, total)
+VALUES(5, 1, 2, 50, 0, 100);
+INSERT INTO compras_has_productos(compras_id, productos_id, cantidad, subtotal, descuento, total)
+VALUES(5, 2, 4, 25, 0, 100);
+
+-- Compra 6
+INSERT INTO compras_has_productos(compras_id, productos_id, cantidad, subtotal, descuento, total)
+VALUES(6, 3, 1, 50, 0, 50);
+INSERT INTO compras_has_productos(compras_id, productos_id, cantidad, subtotal, descuento, total)
+VALUES(6, 15, 2, 70, 0, 140);
+
+-- Compra 7
+INSERT INTO compras_has_productos(compras_id, productos_id, cantidad, subtotal, descuento, total)
+VALUES(7, 8, 2, 65, 0, 130);
+INSERT INTO compras_has_productos(compras_id, productos_id, cantidad, subtotal, descuento, total)
+VALUES(7, 11, 2, 65, 0, 130);
+
+-- Compra 8
+INSERT INTO compras_has_productos(compras_id, productos_id, cantidad, subtotal, descuento, total)
+VALUES(8, 10, 5, 45, 0, 225);
+INSERT INTO compras_has_productos(compras_id, productos_id, cantidad, subtotal, descuento, total)
+VALUES(8, 16, 1, 60, 0, 60);
+
+-- Compra 9
+INSERT INTO compras_has_productos(compras_id, productos_id, cantidad, subtotal, descuento, total)
+VALUES(9, 6, 2, 65, 0, 130);
+INSERT INTO compras_has_productos(compras_id, productos_id, cantidad, subtotal, descuento, total)
+VALUES(9, 14, 2, 70, 0, 140);
+
+-- Compra 10
+INSERT INTO compras_has_productos(compras_id, productos_id, cantidad, subtotal, descuento, total)
+VALUES(10, 8, 1, 65, 0, 65);
+INSERT INTO compras_has_productos(compras_id, productos_id, cantidad, subtotal, descuento, total)
+VALUES(10, 12, 2, 80, 0, 160);
