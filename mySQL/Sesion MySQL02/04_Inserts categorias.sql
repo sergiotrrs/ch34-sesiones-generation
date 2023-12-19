17b4ce4b4b7d5fe4fe30d8c8b0d4bf7359a76449
Sergio Torres <sergio.torres@idr.edu.mx>
2023-12-19 13:48:09 -0600

    ci: inserts de Rafa

--- mySQL/Sesion MySQL02/04_Inserts categorias.sql
+++ mySQL/Sesion MySQL02/04_Inserts categorias.sql
@@ -1,15 +1,15 @@
-
+-- DELETE FROM categorias;
+-- ALTER TABLE categorias AUTO_INCREMENT = 1;
 -- Inserts de tabla categorias
 
-INSERT INTO categorias (nombre, descripcion) VALUES ('Café Arábica', 'Café premium de alta calidad, cultivado en regiones montañosas.');
-INSERT INTO categorias (nombre, descripcion) VALUES ('Café Robusta', 'Café con un sabor fuerte y un alto contenido de cafeína, ideal para amantes del café intenso.');
-INSERT INTO categorias (nombre, descripcion) VALUES ('Café Descafeinado', 'Café procesado para reducir la cafeína, perfecto para aquellos que prefieren un café más suave.');
-INSERT INTO categorias (nombre, descripcion) VALUES ('Café Orgánico', 'Café cultivado sin el uso de pesticidas ni fertilizantes químicos, respetuoso con el medio ambiente.');
-INSERT INTO categorias (nombre, descripcion) VALUES ('Café Molido', 'Café en forma de polvo listo para preparar, conveniente para cafeteras y métodos de preparación tradicionales.');
-INSERT INTO categorias (nombre, descripcion) VALUES ('Café en Grano', 'Café sin moler, ideal para aquellos que prefieren moler su propio café para obtener un sabor fresco.');
-INSERT INTO categorias (nombre, descripcion) VALUES ('Café Gourmet', 'Variedades exclusivas y sabores únicos, para los amantes del café que buscan experiencias sofisticadas.');
-INSERT INTO categorias (nombre, descripcion) VALUES ('Café Flavored', 'Café con sabores añadidos como vainilla, caramelo o chocolate, perfecto para quienes disfrutan de variedades aromatizadas.');
-INSERT INTO categorias (nombre, descripcion) VALUES ('Café Single Origin', 'Café proveniente de una región específica, destacando las características únicas de su origen.');
-INSERT INTO categorias (nombre, descripcion) VALUES ('Café Cold Brew', 'Café preparado en frío durante un período prolongado, ofreciendo un sabor suave y menos acidez.');
+INSERT INTO categorias (nombre, descripcion) VALUES ('Café', 'Variedades de café caliente');
+INSERT INTO categorias (nombre, descripcion) VALUES ('Bebidas', 'Refrescos y bebidas frías');
+INSERT INTO categorias (nombre, descripcion) VALUES ('Frappés', 'Bebidas frías y espumosas');
+INSERT INTO categorias (nombre, descripcion) VALUES ('Especialidades', 'Productos especiales del chef');
+INSERT INTO categorias (nombre, descripcion) VALUES ('Bagels', 'Panecillos de bagel con diversos rellenos');
+INSERT INTO categorias (nombre, descripcion) VALUES ('Paninis', 'Sandwiches calientes prensados');
+INSERT INTO categorias (nombre, descripcion) VALUES ('Cuernitos', 'Productos de panadería en forma de cuerno');
+INSERT INTO categorias (nombre, descripcion) VALUES ('Waffles', 'Waffles dulces y salados');
+INSERT INTO categorias (nombre, descripcion) VALUES ('Panes', 'Variedades de pan fresco');
 
-SELECT id, nombre FROM categorias;
+SELECT * FROM categorias;
--- mySQL/Sesion MySQL02/05_Inserts productos.sql
+++ mySQL/Sesion MySQL02/05_Inserts productos.sql
@@ -1,34 +1,14 @@
 
--- Productos para la categoría 'Café Arábica'
-INSERT INTO productos (nombre, precio, categoria_id) VALUES ('Café Arábica Premium', 12.99, 1);
-INSERT INTO productos (nombre, precio, categoria_id) VALUES ('Café Arábica Suave', 10.99, 1);
 
--- Productos para la categoría 'Café Descafeinado'
-INSERT INTO productos (nombre, precio, categoria_id) VALUES ('Café Descafeinado Clásico', 11.49, 3);
-INSERT INTO productos (nombre, precio, categoria_id) VALUES ('Café Descafeinado Vainilla', 13.99, 3);
-
--- Productos para la categoría 'Café Orgánico'
-INSERT INTO productos (nombre, precio, categoria_id) VALUES ('Café Orgánico Puro', 14.99, 4);
-INSERT INTO productos (nombre, precio, categoria_id) VALUES ('Café Orgánico Mezcla Especial', 16.49, 4);
-
--- Productos para la categoría 'Café Molido'
-INSERT INTO productos (nombre, precio, categoria_id) VALUES ('Café Molido Tradicional', 9.99, 5);
-INSERT INTO productos (nombre, precio, categoria_id) VALUES ('Café Molido Fino', 11.99, 5);
-
--- Productos para la categoría 'Café en Grano'
-INSERT INTO productos (nombre, precio, categoria_id) VALUES ('Café en Grano Premium', 13.99, 6);
-INSERT INTO productos (nombre, precio, categoria_id) VALUES ('Café en Grano Intenso', 15.49, 6);
-
--- Productos para la categoría 'Café Flavored'
-INSERT INTO productos (nombre, precio, categoria_id) VALUES ('Café Sabor Vainilla', 14.99, 8);
-INSERT INTO productos (nombre, precio, categoria_id) VALUES ('Café Sabor Caramelo', 14.99, 8);
-
--- Productos para la categoría 'Café Single Origin'
-INSERT INTO productos (nombre, precio, categoria_id) VALUES ('Café Single Origin Etiopía', 17.99, 9);
-INSERT INTO productos (nombre, precio, categoria_id) VALUES ('Café Single Origin Colombia', 18.49, 9);
-
--- Productos para la categoría 'Café Cold Brew'
-INSERT INTO productos (nombre, precio, categoria_id) VALUES ('Café Cold Brew Clásico', 15.99, 10);
-INSERT INTO productos (nombre, precio, categoria_id) VALUES ('Café Cold Brew Menta', 16.49, 10);
+INSERT INTO productos (nombre, precio, categoria_id) VALUES ('Café Americano', 50, 1);
+INSERT INTO productos (nombre, precio, categoria_id) VALUES ('Espresso', 25, 1);
+INSERT INTO productos (nombre, precio, categoria_id) VALUES ('Matcha Latte', 50, 2);
+INSERT INTO productos (nombre, precio, categoria_id) VALUES ('Chai Latte', 55, 2);
+INSERT INTO productos (nombre, precio, categoria_id) VALUES ('Chocolate', 45, 2);
+INSERT INTO productos (nombre, precio, categoria_id) VALUES ('Café Frappé', 65, 3);
+INSERT INTO productos (nombre, precio, categoria_id) VALUES ('Oreo Frappé', 65, 3);
+INSERT INTO productos (nombre, precio, categoria_id) VALUES ('Mocha Frappé', 65, 3);
+INSERT INTO productos (nombre, precio, categoria_id) VALUES ('Sea Salt Caramel', 60, 3);
+INSERT INTO productos (nombre, precio, categoria_id) VALUES ('Dark Horchata Ice', 55, 3);
 
 SELECT * FROM productos;
