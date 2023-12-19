66c726fc4d1f9ba9cb6fc662007cfaa7f6a2fd81
Sergio Torres <sergio.torres@idr.edu.mx>
2023-12-19 13:50:31 -0600

    ci: 10 nuevos inserts de productos

--- mySQL/Sesion MySQL02/05_Inserts productos.sql
+++ mySQL/Sesion MySQL02/05_Inserts productos.sql
@@ -11,4 +11,15 @@ INSERT INTO productos (nombre, precio, categoria_id) VALUES ('Mocha Frappé', 65
 INSERT INTO productos (nombre, precio, categoria_id) VALUES ('Sea Salt Caramel', 60, 3);
 INSERT INTO productos (nombre, precio, categoria_id) VALUES ('Dark Horchata Ice', 55, 3);
 
+INSERT INTO productos (nombre, precio, categoria_id) VALUES ('Bagel Clásico', 65, 4);
+INSERT INTO productos (nombre, precio, categoria_id) VALUES ('Bagel Serrano', 80, 4);
+INSERT INTO productos (nombre, precio, categoria_id) VALUES ('Bagel Dulce/Salado', 40, 4);
+INSERT INTO productos (nombre, precio, categoria_id) VALUES ('Panini Italiano', 70, 5);
+INSERT INTO productos (nombre, precio, categoria_id) VALUES ('Pizanini', 70, 5);
+INSERT INTO productos (nombre, precio, categoria_id) VALUES ('Panini Vegetariano', 70, 5);
+INSERT INTO productos (nombre, precio, categoria_id) VALUES ('Cuernito Clásico', 65, 6);
+INSERT INTO productos (nombre, precio, categoria_id) VALUES ('Cuernito Italiano', 65, 6);
+INSERT INTO productos (nombre, precio, categoria_id) VALUES ('Waffle Tradicional', 80, 7);
+INSERT INTO productos (nombre, precio, categoria_id) VALUES ('Waffle con Helado o Fresas', 90, 7);
+
 SELECT * FROM productos;
