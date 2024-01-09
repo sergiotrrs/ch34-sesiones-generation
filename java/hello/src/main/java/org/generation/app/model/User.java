fe0239c0b53e833949a7544e5585022e7644fe5e
Sergio Torres <sergio.torres@idr.edu.mx>
2024-01-09 10:52:13 -0600

    ci: implementar interfaz serializable

--- java/hello/src/main/java/org/generation/app/model/User.java
+++ java/hello/src/main/java/org/generation/app/model/User.java
@@ -1,9 +1,13 @@
 package org.generation.app.model;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-public class User {
+public class User implements Serializable {
+	
+
+	private static final long serialVersionUID = 1L;
 	private String firstname;
 	private String lastname;
 	private int cohorte;
