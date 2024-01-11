af81db92f8598c3a2cf709e3d61fb37fdfb7ac69
Sergio Torres <sergio.torres@idr.edu.mx>
2024-01-11 11:37:54 -0600

    ci: conexión a mySQL

---
+++ java/eclipse/sp3_ecommerce/src/main/resources/import.sql
@@ -0,0 +1,2 @@
+INSERT INTO usuarios(id, nombre, apellido, fecha_nacimiento, email) VALUES(1,"Bob", "Esponja", "1986-07-14", "bob@nickelodeon.com");
+INSERT INTO usuarios(id, nombre, apellido, fecha_nacimiento, email) VALUES(2,"Patricio", "Estrella", "1986-07-15", "patricio@nickelodeon.com");
