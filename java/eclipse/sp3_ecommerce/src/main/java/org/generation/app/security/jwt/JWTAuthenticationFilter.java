aa5d1892837c66fb9488e74e9a867e4ba0fe0225
Sergio Torres <sergio.torres@idr.edu.mx>
2024-01-16 15:50:16 -0600

    feat: successfulAuthentication

--- java/eclipse/sp3_ecommerce/src/main/java/org/generation/app/security/jwt/JWTAuthenticationFilter.java
+++ java/eclipse/sp3_ecommerce/src/main/java/org/generation/app/security/jwt/JWTAuthenticationFilter.java
@@ -2,6 +2,8 @@ package org.generation.app.security.jwt;
 
 import java.io.IOException;
 
+import org.generation.app.security.UserDetailsImpl;
+import org.json.JSONObject;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
@@ -9,12 +11,14 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
-	
+	// STEP 7.1 recuperar el email y password del body de la solicitud que vendría en localhost:8080/login
 	@Override
 	public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
 			throws AuthenticationException {
@@ -41,4 +45,30 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
 		
 	}
 	
+	
+	// STEP 7.2 Si la autenticación fue correcta agregamos el token a la respuesta
+	@Override
+	protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
+			Authentication authResult) throws IOException, ServletException {
+		
+		UserDetailsImpl userDetails = (UserDetailsImpl) authResult.getPrincipal();
+		
+		String token = "Hola, Soy el Token"; // TODO Crear token con las librerías de JJWT
+				
+		// Opcional: crear un objeto JSON para la respuesta
+		JSONObject jsonResponse = new JSONObject();
+		jsonResponse.put("token", token);
+		
+		// Configuración de la respuesta http
+		response.setContentType("application/json");
+		response.setCharacterEncoding("UTF-8");
+		response.addHeader("Authorization", "Bearer " + token);
+		
+		// Opcional: Establecer el objeto JSON en el body de la respuesta
+		response.getWriter().write( jsonResponse.toString() );
+		response.getWriter().close();
+		
+		super.successfulAuthentication(request, response, chain, authResult);
+		
+	}
 }
