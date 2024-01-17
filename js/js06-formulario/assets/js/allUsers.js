13a06807ee73162465b818a4871d935ea53034e4
Sergio Torres <sergio.torres@idr.edu.mx>
2024-01-17 14:27:09 -0600

    feat: allUsers

---
+++ js/js06-formulario/assets/js/allUsers.js
@@ -0,0 +1,41 @@
+console.log("JS06- Login");
+
+const enviarDatosAlServidor = async( token  ) => {
+
+
+    console.table( token );
+    
+    const url = "https://sp3-eccomerce.onrender.com//api/v2/users";
+
+    try {
+        const response = await fetch( url ,{
+            method: "GET",
+            headers: { 
+                "Authorization":`Bearer ${token}`
+            }
+        });
+        console.log(response);
+        const allUsers = await response.json();
+        console.log( allUsers );
+        // TODO imprimir en el frontEnd
+        alert("Solicitud exitosa");
+        
+    } catch (error) {
+        console.error( error );
+        alert("Error en la petición");
+    }
+
+
+}
+
+const leerToken = () =>{
+    const token = localStorage.getItem("token");
+    if ( token!== null || token!= undefined){
+        enviarDatosAlServidor( token );
+    } else {
+        alert("Tienes que generar tu token primero");
+    }
+
+}
+
+leerToken();
